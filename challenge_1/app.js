b6fb4690b0492112d9b2242f9f134c3d71ee8890
Alex Miner <alex.miner@gmail.com>
2018-12-10 11:43:12 -0800

    Implement HTML gameboard and started click handlers for td's

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,27 @@
 
 
+// populate board
+
+
+//add event handlers for entire board
+var gameTable = document.getElementById('gameBoard');
+// var rows = gameTable.getElementByTagName("tr");
+// for (var i = 0; i < rows.length; i++) {
+// 	var currentRow = rows[i];
+// }
+
+//do I even have to do this, I think I can just access all td's and jsut add eventhandlers to each of them
+var tableElements = document.getElementsByTagName("td");
+for (var k = 0; k < tableElements.length; k++) {
+	console.log('test inside for loop');
+	currentTd = tableElements[k];
+	currentTd.innerHTML = "hi"; //this is currently working
+}
+// handle user clicks and place pieces alternatingly
+
+// switch turns for placement
+
+// after each piece is placed, make a check to see if someone has won or if there is a tie
+	// when someone wins display the winner
+
+// have a new game button that clears the game and resets board state
