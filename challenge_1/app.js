4de7dd80c157fbe3f1247b0baaf65471bad286ba
Alex Miner <alex.miner@gmail.com>
2018-12-10 16:03:59 -0800

    working implementation

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,18 +3,23 @@ var gameTable = document.getElementById('gameBoard');
 
 var piece = 'x';
 var piecesPlaced = 0;
+var isWinner = false;
 document.getElementById('current-player').innerHTML = (`Current Player: ${piece}`);
 var consoleLoggingEventListener = function () {
 	// only able to place a piece if there is nothing in the tile
-	if (this.innerHTML === '') {
-
+	if (this.innerHTML === '' || isWinner === true) {
 
 		// alternate pieces
 		this.innerHTML = piece;
 
 		// evaluate game on just placed piece
-		evaluteGameState(event.target.id, piece);
-
+		isWinner = evaluteGameState(event.target.id, piece);
+		if (isWinner) {
+			document.getElementById('current-player').innerHTML = (`Current Player: ${piece} is the winner!`);
+			// alert(piece.toUpperCase(), ' is the winner');
+			//turn off ability to place pieces
+			return;
+		}
 		//alternate piece type
 		if (piece === 'x') {
 			piece = 'o';
@@ -23,15 +28,8 @@ var consoleLoggingEventListener = function () {
 		}
 		
 		//show current player
-		if (piecesPlaced < 8) {
-			document.getElementById('current-player').innerHTML = (`Current Player: ${piece}`);
-		} else {
-			document.getElementById('current-player').innerHTML = (`Draw!`);
-		}
-		piecesPlaced++;
-		//after each placement, check board for winning condition
+		document.getElementById('current-player').innerHTML = (`Current Player: ${piece}`);
 		
-		//potentially remove event listener off of thing that clicked it to prevent further clicks		
 	}	
 }
 
@@ -50,11 +48,10 @@ var currentPlayer = function() {
 }
 
 // after each piece is placed, make a check to see if someone has won or if there is a tie
-	// when someone wins display the winner
+// when someone wins display the winner
 var game = [[[''],[''],['']],[[''],[''],['']],[[''],[''],['']]];
 var evaluteGameState = function (locationString, value) {
 	var locationArr = locationString.split(',');
-	// console.log('placed at ', JSON.stringify(locationArr), 'with a ', value);
 
 	// have an internal version of game state
 	
@@ -62,25 +59,65 @@ var evaluteGameState = function (locationString, value) {
 	var x = locationArr[0];
 	var y = locationArr[1];
 	game[x][y] = value;
-	// console.log(value)
-	console.log(game);
+
 	// after each placement
 	//iterate through x row and check if all values match
 	var winner = false;
 	var rowMatches = 0
 	for(var j = 0; j < 3; j++) {
 		if (value === game[x][j]){
-			rowMatches++
+			rowMatches++;
 		} 
 		if (rowMatches === 3) {
-			var winner = true;
-			console.log('you are a winrar!');
+			winner = true;
+			console.log(value, ', you are a winrar!');
 		}
 	}
 
-	
+	//iterate through y column and check if all values match
+	var colMatches = 0
+	for (var i = 0; i < 3; i++) {
+		if (value === game[i][y]){
+			colMatches++;
+		}
+		if (colMatches === 3) {
+			winner === true;
+			console.log(value, ', you are a winrar!');
+		}
+	}
+
+	//check diagonals
+	var diagMain = 0;
+	var diagMin = 0;
+	for (var k = 0; k < 3; k++) {
+
+		if (value === game[k][k]) {
+			diagMain++;
+		}
+		if (value === game[2-k][k]) {
+			diagMin++;
+		}
+		if (diagMain === 3 || diagMin === 3) {
+			winner = true;
+			console.log(value, ', you are a winrar!');
+		}
+	}
+
 	// also check if board is full
+	var boardFullness = 0;
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			if (game[i][j] === 'x' || game[i][j] === 'o')
+				boardFullness++;
+		}
+	}
+	if (boardFullness === 9 && winner === false) {
+		console.log('Game is a draw :(');
+	}
+	console.log(boardFullness);
 	// when condition is met, declare who is the winner and prompt with a reset
+	console.log(winner);
+	return winner;
 }
 
 
