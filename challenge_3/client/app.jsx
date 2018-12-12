f53dfdde839dc33b5f1ad051f135b14ed4081442
Alex Miner <alex.miner@gmail.com>
2018-12-12 14:52:32 -0800

    Initial Commit

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,38 @@
 
 
+
+class App extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			name: '',
+			email: '',
+			password: '',
+			address: {
+				line1: '',
+				line2: '',
+				city: '',
+				state: '',
+				zipcode: ''
+			},
+			phoneNumber: '',
+			creditCard: '',
+			expiryDate: '',
+			cvv: '',
+			billingZipCode: ''
+
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>This is Testing </h1>
+			</div>
+		)
+	};
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+/* Require Statements */
 
+const express = require('express');
+const app = express();
+const PORT = 8081;
+// Serve up static pages
+app.use(express.static('public'));
 
+app.get('/', (req,res) => {
+	res.send('hello world');
+});
+
+app.listen(PORT, () => console.log(`multistep checkout app listening on port ${PORT}!`));
